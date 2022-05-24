2dab33b65464a88d80f73f42daa59961f4f3a1d8
OtterlyTiffin <otterlytiffin@gmail.com>
2022-05-24 19:58:58 +0100

    Setup a backwall & Skirting Board

---
+++ Assets/Scripts/InteractablesScripts.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractablesScripts : MonoBehaviour
+{
+    public InteractiblesSO interactiblesSO;
+    string objectName;
+
+    void Start()
+    {
+        objectName = interactiblesSO.interactableName;
+    }
+
+    public string GetName()
+    {
+        return(interactiblesSO.interactableName);
+    }
+
+}
