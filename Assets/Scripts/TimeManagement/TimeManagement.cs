90745893f89bc47ae137023de50ce85a53c0eca6
Fictional <fictionalaroma@gmail.com>
2022-05-28 22:27:44 +0100

    Add Day Tracker

--- Assets/Scripts/TimeManagement/TimeManagement.cs
+++ Assets/Scripts/TimeManagement/TimeManagement.cs
@@ -20,6 +20,7 @@ namespace Assets.Scripts.TimeManagement
 
         public TextMeshProUGUI timeOfDayText;
         public TextMeshProUGUI debugText;
+        public int dayNumber = 1;
 
         public void Start()
         {
@@ -30,7 +31,7 @@ namespace Assets.Scripts.TimeManagement
             jukebox.PlayNewTrackImmediate(currentDayValues.musicTracks.FirstOrDefault(), true);
 
 
-            timeOfDayText.text = currentDayValues.timeName;
+            SetDayText(currentDayValues);
             _currentTimeSegmentTimer = 0;
         }
 
@@ -43,6 +44,7 @@ namespace Assets.Scripts.TimeManagement
                 if (_dayTimeIndex >= daySegments.Count)
                 {
                     _dayTimeIndex = 0;
+                    dayNumber++;
                 }
 
                 currentDayValues = daySegments[_dayTimeIndex];
@@ -61,8 +63,11 @@ namespace Assets.Scripts.TimeManagement
                 jukebox.SetTracks(newDayValues.musicTracks, newDayValues.playNewTrackImmediately);
             }
 
-            timeOfDayText.text = newDayValues.timeName;
+            timeOfDayText.text = $"Day {dayNumber} - {newDayValues.timeName}";
             _currentTimeSegmentTimer = 0;
         }
+
+        private void SetDayText(TimeOfDayValues newDayValues) => timeOfDayText.text = $"Day {dayNumber} - {newDayValues.timeName}";
+
     }
 }
