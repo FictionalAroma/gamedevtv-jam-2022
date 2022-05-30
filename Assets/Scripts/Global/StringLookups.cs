96ce47e9a4c25b795bb13c9cb47dc2b7deab62fe
Fictional <fictionalaroma@gmail.com>
2022-05-30 13:36:55 +0100

    ITEM UPDATES!

---
+++ Assets/Scripts/Global/StringLookups.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Global
+{
+    public static class Tags
+    {
+        public static string PickupTag => "Pickup";
+
+        public static string InteractablesTag => "CatInteraction";
+    }
+}
--- Assets/Scripts/Interactables/CatInteraction.cs
+++ Assets/Scripts/Interactables/CatInteraction.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text;
 using Assets.Scripts;
 using Assets.Scripts.Global;
@@ -11,17 +12,26 @@ public class CatInteraction : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        var triggeredObject = other.GetComponent<InteractablesScripts>();
-        if (triggeredObject != null )
+        var thisCollider = GetComponent<CharacterController>();
+        if (other.CompareTag(Tags.InteractablesTag))
         {
-            currentInteractablesObject = triggeredObject;
-            Debug.Log(triggeredObject.objectName);
+            var triggeredObject = other.GetComponent<InteractablesScripts>();
+            if (triggeredObject != null)
+            {
+                currentInteractablesObject = triggeredObject;
+                Debug.Log(triggeredObject.objectName);
 
-            StringBuilder stringBuilder = new StringBuilder(triggeredObject.objectName);
-            triggeredObject.events.ForEach(action => stringBuilder.AppendLine(action.catAction.ToString()));
-            CatFadingTextController.Instance.StartCatTalk(stringBuilder.ToString());
-            var attributes = GetComponent<CatAttributes>();
-            triggeredObject.DisplayInteractibleMenu(attributes);
+                StringBuilder stringBuilder = new StringBuilder(triggeredObject.objectName);
+                triggeredObject.events.ForEach(action => stringBuilder.AppendLine(action.catAction.ToString()));
+                CatFadingTextController.Instance.StartCatTalk(stringBuilder.ToString());
+                var attributes = GetComponent<CatAttributes>();
+                triggeredObject.DisplayInteractibleMenu(attributes);
+            }
+        }
+        else if (other.CompareTag(Tags.PickupTag))
+        {
+            var test = thisCollider.
+            test.
         }
     }
 
