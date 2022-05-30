d48048aca7f10ac350c929fdb0291997e3842cc1
Fictional <fictionalaroma@gmail.com>
2022-05-30 07:34:23 +0100

    Add offset for menu start point

--- Assets/InteractionMenuHandler.cs
+++ Assets/InteractionMenuHandler.cs
@@ -7,7 +7,9 @@ public class InteractionMenuHandler : MonoBehaviour
     public static InteractionMenuHandler Instance;
 
     public Canvas actionCanvas;
-    public GameObject actionButtonPrefab; 
+    public GameObject actionButtonPrefab;
+
+    public Vector2 menuStartPosition;
     private void Awake()
     {
         if (Instance == null)
